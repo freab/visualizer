806ccac0a8cd24e31c1fefd677856d3f799939ae
freab <fremesfin1@gmail.com>
2024-02-13 20:15:21 +0300

    fix colors and added postprocessing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React, { useRef } from "react";
 import { Canvas } from "@react-three/fiber";
+import {
+  Vignette,
+  EffectComposer,
+  SMAA,
+  SSAO,
+  ToneMapping,
+} from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
 import { OrbitControls, Loader } from "@react-three/drei";
 import { Rophnan } from "./components/Rophnan";
 import { Suspense } from "react";
@@ -9,25 +17,33 @@ function App() {
     <>
       <Canvas shadows>
         <OrbitControls />
-        <color attach="background" args={["#ececec"]} />
-        <ambientLight intensity={3} color="#7C6033" />
+        <color attach="background" args={["#000000"]} />
+        <ambientLight position={[0, 0, 100]} intensity={1.5} color="#f2c96d" />
         <pointLight
-          position={[22.061, 23.144, 10.194]}
+          position={[10, 10, 10]}
           decay={0}
           color="#dbd59a"
-          intensity={0.519}
+          intensity={0.319}
         />
-        <spotLight
-          position={[1.061, 19.144, 0.194]}
-          angle={Math.PI / 4}
-          penumbra={1}
-          intensity={0.83}
-          color="#896111"
-          castShadow
-        />
+
         <Suspense fallback={null}>
           <Rophnan />
         </Suspense>
+        <EffectComposer>
+          <SSAO
+            blendFunction={BlendFunction.MULTIPLY} // blend mode
+            samples={30} // amount of samples per pixel (shouldn't be a multiple of the ring count)
+            rings={4} // amount of rings in the occlusion sampling pattern
+            distanceThreshold={1.0} // global distance threshold at which the occlusion effect starts to fade out. min: 0, max: 1
+            distanceFalloff={0.0} // distance falloff. min: 0, max: 1
+            rangeThreshold={0.5} // local occlusion range threshold at which the occlusion starts to fade out. min: 0, max: 1
+            rangeFalloff={0.1} // occlusion range falloff. min: 0, max: 1
+            luminanceInfluence={0.9} // how much the luminance of the scene influences the ambient occlusion
+            radius={20} // occlusion sampling radius
+            scale={0.5} // scale of the ambient occlusion
+            bias={0.5} // occlusion bias
+          />
+        </EffectComposer>
       </Canvas>
       <Loader />
     </>
