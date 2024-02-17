72e2746163837ae4f8731b472557502faabafd32
freab <fremesfin1@gmail.com>
2024-02-17 04:50:49 +0300

    remove postprosessing

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { Suspense } from "react";
 import { Sphere } from "./components/Sphere";
 
 function App() {
-  // Lift the state up to App component
   const [selectedSong, setSelectedSong] = useState(
     "ROPHNAN-KING-KUT-Ft-Tom-Beats.m4a"
   );
@@ -16,7 +15,6 @@ function App() {
   return (
     <>
       <Canvas shadows>
-        {/* <OrbitControls /> */}
         <color attach="background" args={["#000000"]} />
         <ambientLight position={[0, 0, 100]} intensity={1.2} color="#f2c96d" />
         <pointLight
@@ -27,7 +25,6 @@ function App() {
         />
 
         <Suspense fallback={null}>
-          {/* Pass selectedSong and setSelectedSong as props */}
           <Rophnan
             selectedSong={selectedSong}
             setSelectedSong={setSelectedSong}
@@ -38,21 +35,6 @@ function App() {
           />
         </Suspense>
 
-        {/* <EffectComposer>
-          <SSAO
-            blendFunction={BlendFunction.MULTIPLY}
-            samples={30}
-            rings={4}
-            distanceThreshold={1.0}
-            distanceFalloff={0.0}
-            rangeThreshold={0.5}
-            rangeFalloff={0.1}
-            luminanceInfluence={0.9}
-            radius={20}
-            scale={0.5}
-            bias={0.5}
-          />
-        </EffectComposer> */}
         <Stats />
       </Canvas>
       <Loader />
